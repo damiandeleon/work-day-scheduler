bdb9b708f5d5e7c3aa8a075ce3d838fcaa41ef9f
damian.deleon <damianmdeleon@gmail.com>
2021-01-23 17:17:05 -0600

    updated code to further develop row shells

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-var createDiv = $("<div>");
-var timeBlock = $(".time-block");
-var description = $(".description");
-var row = $(".row");
-var hour = $(".hour")
+
 
 //set code for the current day using moment
 var rightNow = moment().format("dddd, MMMM Do YYYY");
@@ -13,14 +9,19 @@ $("#currentDay").append(rightNow);
 //use if statement to set the timeblocks backgrounds to change depending on the chornological relatinoship each block is to the current time (past, current, future)
 var workHoursArr = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 
-for (let i = 0; i < workHoursArr.length; i++){
-    // create the rows in table format
-    var createDiv = $("<tr>").prepend(workHoursArr[i]);
-    //append the rows to the parent container
-    let newRow = $(".container").append(createDiv);
-    //give each row a time-block class for formatting purposes
-    $(newRow[i]).addClass("time-block");
-    $("tr").addClass("past");
-  
-    
-}
+
+$(".container").append("<div class='row'></div>");
+// $(".row").appendTotext("I am a time block");
+$(".row").append("<div class='hour'><I the hour block/div>");
+$(".row").append("<textarea class='description'> I am the text area </textarea>");
+$(".row").append("<div class='saveBtn'> I am the save button </div>");
+
+// for (let i = 0; i < workHoursArr.length; i++){
+//     // create the rows in table format
+//     var createDiv = $("<textarea>").prepend(workHoursArr[i]);
+//     //append the rows to the parent container
+//     let newRow = $(".container").append(createDiv);
+//     //give each row a time-block class for formatting purposes
+//     $(newRow[i]).addClass("time-block");
+      
+// }
