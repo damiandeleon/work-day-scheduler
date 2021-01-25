c39d568e839107a3b3ca6189304d9d37b8ad85c5
damian.deleon <damianmdeleon@gmail.com>
2021-01-25 12:27:09 -0600

    still working on adding a value to each hourOfDay ID

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var year = current.getFullYear();
 var month = current.getMonth()+1;
 var date = current.getDate();
 var hour = current.getHours();
-var hoursID = $("#hour");
+var timeTag = $("time");
 
 
 //set code for the current day using moment
@@ -15,15 +15,16 @@ $("#currentDay").append(rightNow);
 
 
 var workHoursArr = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-
+var militaryTime = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 $(".container").attr("style", "time-block");
+timeValCount = 8;
 //create blocks of time slots 
 for(var i = 0; i < workHoursArr.length; i++){
     //create the row and time-block div
     var rowEl = $("<div class='row'></div>");
     //create the time div
-    var timeEl = $("<div id='hour' class='col-1'></div>").append(workHoursArr[i]);
+    var timeEl = $("<time class='hour col-1' id='hourOfDay'></time>").append(workHoursArr[i]);
      // create the textarea div
     var textareaEl = $("<textarea class='description col-10'></textarea>");
     //create the save Btn div
@@ -37,13 +38,12 @@ for(var i = 0; i < workHoursArr.length; i++){
 }
 
 //create a military time string
-var militaryTime = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
 //loop through the militaryTime array and assign each indix as a value to the timeEl tags
-for(var i = 0; i < hoursID.length; i++){
-//add a value to the timeEl, to track time
-$("#hour").attr("value", 9);
-    
-}
+$('#hourOfDay').each(function(index){
+    //do stuff
+    console.log(index);
+  });
 
 //find code to tell the date and time right now 
 // console.log(year, month, date, hour);
