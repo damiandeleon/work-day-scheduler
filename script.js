68cbd4ef31f18e137297067c698580a00855d638
damian.deleon <damianmdeleon@gmail.com>
2021-01-25 13:31:50 -0600

    started the text area shading if statements

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //establish some useful global variables
 var current = new Date();
 var year = current.getFullYear();
-var month = current.getMonth()+1;
+var month = current.getMonth() + 1;
 var date = current.getDate();
+//find code to tell the time right now 
 var hour = current.getHours();
 var timeTag = $("time");
 
@@ -20,12 +21,12 @@ var workHoursArr = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "
 $(".container").attr("style", "time-block");
 timeValCount = 8;
 //create blocks of time slots 
-for(var i = 0; i < workHoursArr.length; i++){
+for (var i = 0; i < workHoursArr.length; i++) {
     //create the row and time-block div
     var rowEl = $("<div class='row'></div>");
     //create the time div
     var timeEl = $("<time class='hour col-1 hourOfDay'></time>").append(workHoursArr[i]);
-     // create the textarea div
+    // create the textarea div
     var textareaEl = $("<textarea class='description col-10'></textarea>");
     //create the save Btn div
     var saveBtnEl = $("<button class='saveBtn col-1'>Save</button>");
@@ -34,17 +35,29 @@ for(var i = 0; i < workHoursArr.length; i++){
     rowEl.append(textareaEl);
     rowEl.append(saveBtnEl);
     $(".container").append(rowEl);
-    
-}
-    var timeValCount = 9;
-    //loop through the militaryTime array and assign each indix as a value to the timeEl tags
-    $('.hourOfDay').each(function(){
-        $(this).attr("value", timeValCount);
-        timeValCount = timeValCount + 1;
-    });
 
-//find code to tell the date and time right now 
-// console.log(year, month, date, hour);
+}
+var timeValCount = 9;
+//loop through the militaryTime array and assign each indix as a value to the timeEl tags
+$('.hourOfDay').each(function () {
+    $(this).attr("value", timeValCount);
+
+    if ((this)<hour){
+        $(textareaEl).attr("class", "description col-10 future");
+    } else if 
+    ((this)==hour){
+        $(textareaEl).attr("class", "description col-10 present");
+    } else {
+        $(textareaEl).attr("class", "description col-10 past");
+    }
+    timeValCount = timeValCount + 1;
+
+});
+
+
+console.log(hour);
+
+console.log(hour);
 
     //use if statement to set the timeblocks backgrounds to change depending on the chornological relatinoship each block is to the current time (past, current, future)
 
