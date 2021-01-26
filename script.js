8ae02b1336d7a12c334b1b32e3fb2f9c43d8b4cf
damian.deleon <damianmdeleon@gmail.com>
2021-01-26 14:18:33 -0600

    updated the time blocks with class assignment and shading.  Began work on the save button functions

--- script.js
+++ script.js
@@ -25,68 +25,53 @@ $(document).ready(function () {
 
 
     $(".container").attr("style", "time-block");
-    timeValCount = 8;
-    //create blocks of time slots 
-    for (var i = 0; i < workHoursArr.length; i++) {
-        //create the row and time-block div
-        var rowEl = $("<div class='row'></div>");
-        //create the time div
-        var timeEl = $("<time class='hour col-1 hourOfDay'></time>").append(workHoursArr[i]);
-        // create the textarea div
-        var textareaEl = $("<textarea class='description col-10'></textarea>");
-        var inputEl = $("<input type = 'text' name= 'details' id= 'details'/>");
-        //create the save Btn div
-        var saveBtnEl = $("<button class='saveBtn col-1 fa fa-save'></button>");
-        //append each item to the row 
-        rowEl.append(timeEl);
-        rowEl.append(textareaEl);
-        textareaEl.append(inputEl);
-        rowEl.append(saveBtnEl);
-        $(".container").append(rowEl);
 
-    }
-    var timeValCount = 9;
-    //loop through the militaryTime array and assign each indix as a value to the timeEl tags
-    $('.hourOfDay').each(function () {
-        $(".hourOfDay").attr("value", timeValCount);
-        timeValCount = timeValCount + 1;
-        for (let i = 0; i < 9; i++) {
-            console.log($(".hourOfDay").val());
-            if ($(this).val() < hour) {
+
+
+    //create blocks of time slots 
+    renderblocks();
+    function renderblocks() {
+        function styleElement(element) {
+            //use if statement to set the timeblocks backgrounds to change depending on the chornological relatinoship each block is to the current time (past, current, future)
+
+            if (element.attr('value') < hour) {
                 $(textareaEl).attr("class", "description col-10 past");
             } else if (
-                $(this).val() > hour) {
+                element.attr('value') > hour) {
                 $(textareaEl).attr("class", "description col-10 future");
             }
             else if (
-                $(this).val() == hour) {
+                element.attr('value') == hour) {
                 $(textareaEl).attr("class", "description col-10 present");
             }
         }
-    });
-
-    //use if statement to set the timeblocks backgrounds to change depending on the chornological relatinoship each block is to the current time (past, current, future)
 
 
-    // console.log($(".hourOfDay"));
-    // //loop throuch each timeEl div and access it's value
-    // function chronoShader (){
 
-    // }
-    // chronoShader();
-    // $("time").each(function () {
+        for (var i = 0; i < workHoursArr.length; i++) {
+            //create the row and time-block div
+            var rowEl = $("<div class='row'></div>");
+            //create the time div
+            var timeEl = $("<div class='hour col-1 hourOfDay'></div>").append(workHoursArr[i]);
+            timeEl.attr("value", i + 9);
+            // create the textarea div
+            var textareaEl = $("<textarea class='description col-10'></textarea>");
+            styleElement(timeEl)
+            var inputEl = $("<input type = 'text' name= 'details' >");
+            inputEl.attr('id', i + 9);
+            //create the save Btn div
+            var saveBtnEl = $("<button class='saveBtn col-1 fa fa-save'></button>");
+            //append each item to the row 
+            rowEl.append(timeEl);
+            rowEl.append(textareaEl);
+            textareaEl.append(inputEl);
+            rowEl.append(saveBtnEl);
+            $(".container").append(rowEl);
 
+        }
+        renderDetails();
+    }
 
-    // });
-
-    //     if ($(time).value < hour){
-    //     $(textareaEl).attr("class", "description col-10 future");
-    // } else if 
-    // (.value == hour){
-    //     $(textareaEl).attr("class", "description col-10 present");
-    // } else {
-    //     $(textareaEl).attr("class", "description col-10 past");
-    // }
 
 
 
@@ -95,7 +80,7 @@ $(document).ready(function () {
     //build in local storage save feature inside the saveBtn div
 
     function renderDetails() {
-        var eventDetails = localStorage.getItem("details");
+        var eventDetails = localStorage.getItem("id");
 
         if (!eventDetails) {
             return;
@@ -103,13 +88,14 @@ $(document).ready(function () {
         textDescription.textContent = eventDetails;
     }
 
-    $(saveBtn).on("click", function (event) {
+    $('.saveBtn').on("click", function (event) {
         event.preventDefault();
 
-        var details = document.querySelector("#details").value;
+        localStorage.setItem($(this).prev().children().attr('id'), $(this).prev().val());
+        // var details = document.querySelector("#details").value;
 
-            localStorage.setItem("details", details);
-            renderDetials();
+        //     localStorage.setItem("details", details);
+        //     renderDetials();
     });
 });
     //create a onclick event using jQuery to issue a function to save the information in the text area
