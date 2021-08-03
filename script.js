3671d2eba8fcd22c012df4549147708831fc7bab
Damian De Leon <damianmdeleon@gmail.com>
2021-08-03 13:14:58 -0500

    updated psuudo coding

--- script.js
+++ script.js
@@ -29,41 +29,42 @@ $(document).ready(function () {
 
     //create function that will render the information stored in local storage 
     function renderDetails() {
+        // console.log(localStorage.getItem($('#saveButton6').prev().attr('id')))
 
-        //create variables which will assign the 
-
-        var eventDetails1 = localStorage.getItem($("#sb6").prev().attr('id'));
-
-        var eventDetails2 = localStorage.getItem($("#sb7").prev().attr('id'));
-
-        var eventDetails3 = localStorage.getItem($("#sb8").prev().attr('id'));
-
-        var eventDetails4 = localStorage.getItem($("#sb9").prev().attr('id'));
-
-        var eventDetails5 = localStorage.getItem($("#sb10").prev().attr('id'));
-
-        var eventDetails6 = localStorage.getItem($("#sb11").prev().attr('id'));
-
-        var eventDetails7 = localStorage.getItem($("#sb12").prev().attr('id'));
-
-        var eventDetails8 = localStorage.getItem($("#sb13").prev().attr('id'));
-
-        var eventDetails9 = localStorage.getItem($("#sb14").prev().attr('id'));
-
-        var eventDetails10 = localStorage.getItem($("#sb15").prev().attr('id'));
-
-        var eventDetails11 = localStorage.getItem($("#sb16").prev().attr('id'));
-
-        var eventDetails12 = localStorage.getItem($("#sb17").prev().attr('id'));
-
-        var eventDetails13 = localStorage.getItem($("#sb18").prev().attr('id'));
-
-        var eventDetails14 = localStorage.getItem($("#sb19").prev().attr('id'));
-
-        var eventDetails15 = localStorage.getItem($("#sb20").prev().attr('id'));
+        //for each time slot, create a variable called "eventDetails".  Each "eventDetils variable" will equal the contents of the corresponding local storage.  (Note: navigate the dom by finding the local storage contents of the element tag that comes before the element with the id of "saveButton[n]".  From there find the local storage key that matches the number assigned to "id" of that element. So, if you're looking for #id 6, then you pull the value of the local storage with the key of 6).
 
 
+        var eventDetails1 = localStorage.getItem($("#saveButton6").prev().attr('id'));
 
+        var eventDetails2 = localStorage.getItem($("#saveButton7").prev().attr('id'));
+
+        var eventDetails3 = localStorage.getItem($("#saveButton8").prev().attr('id'));
+
+        var eventDetails4 = localStorage.getItem($("#saveButton9").prev().attr('id'));
+
+        var eventDetails5 = localStorage.getItem($("#saveButton10").prev().attr('id'));
+
+        var eventDetails6 = localStorage.getItem($("#saveButton11").prev().attr('id'));
+
+        var eventDetails7 = localStorage.getItem($("#saveButton12").prev().attr('id'));
+
+        var eventDetails8 = localStorage.getItem($("#saveButton13").prev().attr('id'));
+
+        var eventDetails9 = localStorage.getItem($("#saveButton14").prev().attr('id'));
+
+        var eventDetails10 = localStorage.getItem($("#saveButton15").prev().attr('id'));
+
+        var eventDetails11 = localStorage.getItem($("#saveButton16").prev().attr('id'));
+
+        var eventDetails12 = localStorage.getItem($("#saveButton17").prev().attr('id'));
+
+        var eventDetails13 = localStorage.getItem($("#saveButton18").prev().attr('id'));
+
+        var eventDetails14 = localStorage.getItem($("#saveButton19").prev().attr('id'));
+
+        var eventDetails15 = localStorage.getItem($("#saveButton20").prev().attr('id'));
+
+        //add the text contents of each eventDetail varible into the elements identified with the number corresponding to it's ID.
         $("#6").text(eventDetails1)
         $("#7").text(eventDetails2)
         $("#8").text(eventDetails3)
@@ -81,9 +82,7 @@ $(document).ready(function () {
         $("#20").text(eventDetails15)
     }
 
-
-    //create blocks of time slots by calling 
-
+    //create function rednerblocks to render blocks
     function renderblocks() {
         function styleElement(element) {
             //use if statement to set the timeblocks backgrounds to change depending on the chornological relatinoship each block is to the current time (past, current, future)
@@ -99,43 +98,51 @@ $(document).ready(function () {
                 $(textareaEl).attr("class", "description col-9 present");
             }
         }
-
-
-
+        //create a for loop that cycles through the workHoursArr array so it can populate the contents to each row as they are created
         for (var i = 0; i < workHoursArr.length; i++) {
             //create the row and time-block div
             var rowEl = $("<div class='row'></div>");
-            //create the time div
+            //create the time div element append the contents of the prespective array index from "workHoursArr"
             var timeEl = $("<div class='hour col-1' style = 'padding-top: 30px'></div>").append(workHoursArr[i]);
+
+            //becasue the first hour of the calendar day is 6am, you will take the value and add 6 to it so it renders with the correct value
             timeEl.attr("value", i + 6);
             // create the textarea div
             var textareaEl = $("<textarea class='description col-7'></textarea>");
+
+            //assign an id correlating to the hour of the day represented in that row.
             textareaEl.attr('id', i + 6);
+
+            //call the "styleElement" function to set the background color so that past timeslots appear gray, the current time slot appears red and the future timeslots appear green.
             styleElement(timeEl)
-            //create the save Btn div
+
+            //create the save Btn div element
             var saveBtnEl = $("<button class='saveBtn col-1 far fa-save'></button>");
+
+            //add an id to the newly created called 'saveButton' and the number corresponding to it's time slot
+            saveBtnEl.attr('id', "saveButton" + (i + 6));
+
+            //create a delete Btn div element
             var deleteBtnEl = $("<button class ='deleteBtn col-1 far fa-trash-alt'></button>");
-            saveBtnEl.attr('id', "sb" + (i + 6));
+
             //append each item to the row 
             rowEl.append(timeEl);
             rowEl.append(textareaEl);
             rowEl.append(saveBtnEl);
             rowEl.append(deleteBtnEl);
+
+            //then append the newly completed rowEL to the div with the class of "container"
             $(".container").append(rowEl);
 
         }
+        //after all of the blocks are created, recall local storage to populate the blocks with any saved details it has.  
         renderDetails();
     }
-
+    //create blocks of time slots by calling 
     renderblocks();
 
 
-
-
-    //build in local storage save feature inside the saveBtn div
-
-
-    //create a onclick event using jQuery to issue a function to save the information in the text area
+    //create an onclick event using jQuery to call a function to save the information in the text area div
 
     $('.saveBtn').on("click", function (event) {
         event.preventDefault();
@@ -143,6 +150,8 @@ $(document).ready(function () {
         localStorage.setItem($(this).prev().attr('id'), $(this).prev().val());
   
     });
+
+    //create an onclick event using jQuery to call a function to delete the information in the text area div
 
     $('.deleteBtn').on("click", function (event) {
         event.preventDefault();
